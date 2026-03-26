f6198b6a61d2af544b3eca5d3487b52e111eed47
lunarfrm777 <photoslune8@gmail.com>
2026-03-26 11:16:06 -0400

    oops lol wrong div in FreshBake

--- src/scripts/cart.js
+++ src/scripts/cart.js
@@ -4,10 +4,17 @@ function saveCart() {
   localStorage.setItem("cart", JSON.stringify(cart));
 }
 
+// export function addToCart(name, price) {
+//   cart.push({ name, price });
+//   saveCart();
+//   updateCartCount();
+// }
+//  updated addToCart to directly manipulate local storage without using the cart variable, since the cart variable is not being updated in real time and can lead to inconsistencies when multiple tabs are open or when the page is refreshed.
 export function addToCart(name, price) {
+  const cart = JSON.parse(localStorage.getItem("cart")) || [];
   cart.push({ name, price });
-  saveCart();
-  updateCartCount();
+
+  localStorage.setItem("cart", JSON.stringify(cart));
 }
 
 // returning the current cart in local storage 
