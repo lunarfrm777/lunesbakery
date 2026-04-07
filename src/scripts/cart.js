5d41f6e77cedd066484845b72f6259a8f070638b
lunarfrm777 <photoslune8@gmail.com>
2026-04-07 12:40:41 -0400

    cleaning up cart.js,, added saveCart function after updating an exisitingItem or pushing one to the cart array, updating right after which is used from localstorage to then update the total and price in nav and then once getting to the /cart page

--- src/scripts/cart.js
+++ src/scripts/cart.js
@@ -1,43 +1,37 @@
-let cart = JSON.parse(localStorage.getItem("cart")) || [];
+// returning the current cart in local storage 
 
-function saveCart() {
+export function getCart() {
+  return JSON.parse(localStorage.getItem("cart")) || [];
+}
+
+function saveCart(cart) {
   localStorage.setItem("cart", JSON.stringify(cart));
 }
 
-// export function addToCart(name, price) {
-//   cart.push({ name, price });
-//   saveCart();
-//   updateCartCount();
-// }
-//  updated addToCart to directly manipulate local storage without using the cart variable, since the cart variable is not being updated in real time and can lead to inconsistencies when multiple tabs are open or when the page is refreshed.
-// export function addToCart(name, price) {
-//   const cart = JSON.parse(localStorage.getItem("cart")) || [];
-//   cart.push({ name, price });
 
-//   localStorage.setItem("cart", JSON.stringify(cart));
-// }
-export function addToCart(name, price) {
-  const cart = JSON.parse(localStorage.getItem("cart")) || [];
+export function addToCart(id, name, price) {
+  const cart = getCart();
 
   const existingItem = cart.find(item => item.name === name);
 
   if (existingItem) {
-    // if item already exists → increase qty
+    // increasing exisitngItem by 1 if there
     existingItem.qty += 1;
   } else {
     // otherwise → add new item with id + qty
     cart.push({
-      id: crypto.randomUUID(), // 🔥 unique id
+      // generates unique id
+      id: crypto.randomUUID(),
       name,
       price,
       // qty allows for avoiding duplications in the array of items,, and rather just updating value of key
       qty: 1
     });
   }
-}
-// returning the current cart in local storage 
-export function getCart() {
-  return JSON.parse(localStorage.getItem("cart")) || [];
+
+  saveCart(cart);
+  // updates cart immediately
+  updateCartCount();
 }
 
 export function updateCartCount() {
